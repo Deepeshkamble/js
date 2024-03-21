24795d8aa923e6709a60b4473c5e300412a77415
Deepeshkamble <43680630+Deepeshkamble@users.noreply.github.com>
2024-03-21 15:11:55 +0000

    conversion operation

---
+++ 02-datatypes/conversionOperation.js
@@ -0,0 +1,23 @@
+let score = true
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNum = Number(score)
+console.log(typeof valueInNum);
+console.log(valueInNum);
+
+// "33" = 33
+//"33abc" = NaN
+// true = 1; false = 0
+
+let isLoggedIn = "deepesh"
+let bool = Boolean(isLoggedIn)
+console.log(bool);
+
+//1 = true; 0 = false
+//"" = false
+//"deepesh = true"
+
+let abc = 33
+let abcString = String(abc)
+console.log(typeof abcString);
